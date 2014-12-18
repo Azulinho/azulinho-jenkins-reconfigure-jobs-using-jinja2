fc7d38b8808b0c7651dde1fa622475bbf174aa66
Azul <mail@azulinho.com>
2014-12-18 15:30:04 +0000

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This repo contains ansible code to ...
+This repo contains ansible code to configure jobs in Jenkins using Jinja2 templating
 
 When cloning from github, simply run:
 
@@ -6,4 +6,4 @@ When cloning from github, simply run:
 
 When using galaxy, simply run:
 
-    ansible-galaxy install Azulinho.azulinho-...
+    ansible-galaxy install Azulinho.azulinho-jenkins-reconfigure-jobs-using-jinja2
--- vagrantbox/main_spec.rb
+++ vagrantbox/main_spec.rb
@@ -1,2 +1,23 @@
 require_relative 'spec_helper'
 
+jobs_list = [
+  'jinja2_base_template',
+  'jinja2_deploy_template',
+  'jinja2_deploy_zabbix',
+  'jinja2_deploy_zabbix_checks',
+  'jinja2_example1',
+  'jinja2_example2',
+  'jinja2_example3',
+  'jinja2_example4',
+  'jinja2_example5',
+  'jinja2_example6',
+  'jinja2_run_ansible',
+  'jinja2_run_zabbix_tests'
+]
+
+describe command('sudo java -jar /opt/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080/ list-jobs') do
+  jobs_list.each do |job|
+    its(:stdout) { should include job }
+  end
+end
+
